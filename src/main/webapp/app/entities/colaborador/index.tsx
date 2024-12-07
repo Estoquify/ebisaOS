82dfd0bfeb026ea26c69fd63a9e87a4acd86dd54
StarKhael <mikhaelbarretos@gmail.com>
2024-12-07 19:33:37 -0300

    feat: removi as rotas das entidades colaborador e funcao

--- src/main/webapp/app/entities/colaborador/index.tsx
+++ src/main/webapp/app/entities/colaborador/index.tsx
@@ -10,13 +10,13 @@ import ColaboradorDeleteDialog from './colaborador-delete-dialog';
 
 const ColaboradorRoutes = () => (
   <ErrorBoundaryRoutes>
-    <Route index element={<Colaborador />} />
+    {/* <Route index element={<Colaborador />} />
     <Route path="new" element={<ColaboradorUpdate />} />
     <Route path=":id">
       <Route index element={<ColaboradorDetail />} />
       <Route path="edit" element={<ColaboradorUpdate />} />
       <Route path="delete" element={<ColaboradorDeleteDialog />} />
-    </Route>
+    </Route> */}
   </ErrorBoundaryRoutes>
 );
 
